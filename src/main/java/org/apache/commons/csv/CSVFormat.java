d3ab12bbba50274bedec10c51a9e61c6d422dbb1
thoithoi1 <thoithoi1@yahoo.co.in>
2016-07-03 16:01:13 +0530

    Update CSVFormat.java

--- src/main/java/org/apache/commons/csv/CSVFormat.java
+++ src/main/java/org/apache/commons/csv/CSVFormat.java
@@ -796,7 +796,7 @@ public final class CSVFormat implements Serializable {
         for (int i=0; i<20; i++) {
             System.out.print('.');
             try {
-                Thread.currentThread().sleep(1000);
+                Thread.currentThread().sleep(1);
             } catch (InterruptedException e) {
                 break;
             }
